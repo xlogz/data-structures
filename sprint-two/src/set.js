06e096d351e89edef4a70d08487b74f685ca08a7
Logan <loganxlow@gmail.com>
2015-07-03 18:50:14 -0700

    Completed set.js

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,25 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+  this._storage[item] = item;
 };
 
 setPrototype.contains = function(item){
+  if(this._storage.hasOwnProperty(item)){
+    return true;
+  }else{
+    return false;
+  }
 };
 
 setPrototype.remove = function(item){
+  delete this._storage[item];
 };
 
 /*
